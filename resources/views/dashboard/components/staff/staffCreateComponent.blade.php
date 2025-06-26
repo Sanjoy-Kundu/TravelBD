6e4a6e55aeafbdcd5f5c793ff53c4bb3749684ff
unknown <sanjoykundu187@gmail.com>
2025-06-27 00:58:25 +0600

    staff meddileare has been created successfully

--- resources/views/dashboard/components/staff/staffCreateComponent.blade.php
+++ resources/views/dashboard/components/staff/staffCreateComponent.blade.php
@@ -26,7 +26,8 @@
                     <div class="col-md-12 mb-3">
                         <label for="name">Name</label>
                         <input type="text" class="form-control" id="staff_name" name="name"
-                            placeholder="Enter full name" required>
+                            placeholder="Enter full name">
+                            <span class="text-danger" id="staff_name_error"></span>
                     </div>
                 </div>
 
@@ -35,13 +36,14 @@
                     <div class="col-md-12 mb-3">
                         <label for="email">Email</label>
                         <input type="email" class="form-control" id="staff_email" name="email"
-                            placeholder="Enter email address" required>
+                            placeholder="Enter email address">
+                            <span class="text-danger" id="staff_email_error"></span>
                     </div>
                 </div>
 
                 <!-- Submit Button -->
                 <div class="text-right">
-                    <button type="submit" class="btn btn-success px-4">Save Staff</button>
+                    <button type="submit" class="btn btn-success px-4" onclick="createStaff(event)">Create Staff</button>
                 </div>
             </form>
         </div>
@@ -54,5 +56,38 @@
         if (!token) {
             window.location.href = "/admin/login";
         }
+    }
+
+
+    function createStaff(event) {
+        event.preventDefault();
+        let token = localStorage.getItem('token');
+
+        //span error message clear
+        document.getElementById('staff_email_error').innerHTML = '';
+        document.getElementById('staff_name_error').innerHTML = '';
+
+        let email = document.getElementById('staff_email').value;
+        let name = document.getElementById('staff_name').value;
+        let isError = false
+        
+        if(email == ''){
+            document.getElementById('staff_email_error').innerHTML = 'Email is required';
+            isError = true;
+        }
+        if(name == ''){
+            document.getElementById('staff_name_error').innerHTML = 'Name is required';
+            isError = true;
+        }
+        if(isError){
+            return false;
+        }
+
+        let data = {
+            email: email,
+            name: name,
+        }
+
+        console.log(data);
     }
 </script>
